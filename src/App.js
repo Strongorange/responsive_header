3208d00f49655a87054b1d22627d9c7de9935a07
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-07-23 23:30:39 +0900

    Responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/styles.scss";
+import { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGoogle,
+  faTwitter,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { set } from "harmony-reflect";
 
 function App() {
+  const [active, setActive] = useState(false);
+  const handleActive = () => (active ? setActive(false) : setActive(true));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="header__logo">
+          <FontAwesomeIcon icon={faGoogle} size="2x" />
+          <span>NoruWedding</span>
+        </div>
+        <div className={active ? "header__menus active" : "header__menus"}>
+          <ul>
+            <li>Home</li>
+            <li>Gallery</li>
+            <li>Weddings</li>
+            <li>FAQ</li>
+            <li>Bookings</li>
+          </ul>
+        </div>
+        <div className={active ? "header__links active" : "header__links"}>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </li>
+          </ul>
+        </div>
+
+        <div className="header__toggle">
+          <a href="#">
+            <FontAwesomeIcon icon={faBars} size="2x" onClick={handleActive} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
